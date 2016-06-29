48bd6af5a7f7fb5cafb2fda5cc6cc7544a59841c
bask <xavier@iguiyu.com>
2016-06-29 19:25:13 +0800

    set popupwindow success

--- app/src/main/java/com/tomduan/sharesdk/MainActivity.java
+++ app/src/main/java/com/tomduan/sharesdk/MainActivity.java
@@ -3,11 +3,24 @@ package com.tomduan.sharesdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tomduan.shareroot.BaskShare;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.test)
+    public void test(){
+        BaskShare baskShare = new BaskShare(this);
+        baskShare.show();
     }
 }
--- shareroot/src/main/java/com/tomduan/shareroot/BaskShare.java
+++ shareroot/src/main/java/com/tomduan/shareroot/BaskShare.java
@@ -1,25 +1,42 @@
 package com.tomduan.shareroot;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 
 /**
  * Created by bask on 6/29/16.
  */
-public class BaskShare extends View{
+public class BaskShare {
+    private static final String BASKSHARE = "Bask Share";
 
-    public BaskShare(Context context) {
-        super(context);
+    private Activity mActivity;
+    private PopupWindow mPopupWindow;
+
+    public BaskShare(Activity activity) {
+        this.mActivity = activity;
+        initView(mActivity);
     }
 
-    public BaskShare(Context context, AttributeSet attrs) {
-        super(context, attrs);
+    private void initView(Context context) {
+        View rootView = LayoutInflater.from(context).inflate(R.layout.container, null);
+        mPopupWindow = new PopupWindow(rootView);
+        mPopupWindow.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+        mPopupWindow.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
     }
 
-    public BaskShare(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public void show(){
+        mPopupWindow.setFocusable(true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+        mPopupWindow.showAtLocation(this.mActivity.getWindow().getDecorView(), 80, 0, 0);
+        Log.i(BASKSHARE, this.mActivity.getWindow().getDecorView().toString());
     }
-
 
 }
