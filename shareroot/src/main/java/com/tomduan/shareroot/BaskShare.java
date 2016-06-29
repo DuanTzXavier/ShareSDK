3fd4c091ed0a004da517b18a26a1d2b2d2ddb04f
bask <xavier@iguiyu.com>
2016-06-29 18:07:55 +0800

    first commit

---
+++ shareroot/src/main/java/com/tomduan/shareroot/BaskShare.java
@@ -0,0 +1,25 @@
+package com.tomduan.shareroot;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bask on 6/29/16.
+ */
+public class BaskShare extends View{
+
+    public BaskShare(Context context) {
+        super(context);
+    }
+
+    public BaskShare(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaskShare(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
